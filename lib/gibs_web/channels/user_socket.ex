697dc292f64ce817fb7b8ec7aa1a796b62c5ffe1
Matt Neel <requisite.zero@gmail.com>
2018-12-29 17:12:26 -1000

    Mock channels.

--- lib/gibs_web/channels/user_socket.ex
+++ lib/gibs_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule GibsWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", GibsWeb.RoomChannel
+  channel "lobby:*", GibsWeb.LobbyChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
